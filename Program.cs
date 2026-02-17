462fdc9a8f6f22fe95db503ce5ea7ac82fba0655
Alex Romanov <romy63ru@hotmail.com>
2026-02-17 02:05:40 +0100

    Add middleware to remove security headers in non-development environments

--- Program.cs
+++ Program.cs
@@ -15,6 +15,20 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers.Remove("X-Frame-Options");
+            context.Response.Headers.Remove("Content-Security-Policy");
+            return Task.CompletedTask;
+        });
+
+        await next();
+    });
+}
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
